d9be162c4e9e7f1bc16526e5508eb8a8b8fc002d
unknown <rakunnaka@gmail.com>
2025-02-13 09:16:16 +0700

    update

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    function auth(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'message' => 'wromg password',
+            ],);
+        }
+
+        $token = $user->createToken($user->name . '-authToken')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login Success',
+            'access_token' => $token,
+        ]);
+    }
+    
+    function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return response([
+            'message' => 'register successfully',
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,29 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BannerController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductTypeController;
+use App\Models\Banner;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('product-types', ProductTypeController::class);
-Route::resource('product', ProductController::class);
-Route::resource('banner', BannerController::class);
-Route::post('product-update/{id}', [ProductController::class, 'update']);
-Route::post('banner-update/{id}', [BannerController::class, 'update']);
+Route::resource('product-type', ProductTypeController::class);
+Route::get('product', [ProductController::class, 'index']);
+Route::get('product/{id}', [ProductController::class, 'show']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('auth', [AuthController::class, 'auth']);
 
+Route::group([
+    'middleware' => 'auth:sanctum'
+], function () {
+    Route::post('product', [ProductController::class, 'store']);
+    Route::post('product-update/{id}', [ProductController::class, 'update']);
+});
+
+Route::resource('banner', BannerController::class);
+Route::post('banner-update/{id}', [BannerController::class, 'update']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
